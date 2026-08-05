b3b19798dad08cee2db998c4e4ae4c515bdf05a5
corbonzo1212 <corbin.flower11@gmail.com>
2026-08-05 17:33:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this version any time you change app.js/styles.css/index.html
 // so returning users get the new files instead of a stale cache.
-const CACHE_NAME = "tote-tracker-v5";
+const CACHE_NAME = "tote-tracker-v7";
 
 const APP_SHELL = [
   "./",
